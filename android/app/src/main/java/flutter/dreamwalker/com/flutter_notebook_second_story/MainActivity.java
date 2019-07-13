fc251d086b324ad6a47fd9935d7392ab5c754756
Dreamwalker <aristojeff@gmail.com>
2019-07-13 16:38:45 +0900

    Adds First

---
+++ android/app/src/main/java/flutter/dreamwalker/com/flutter_notebook_second_story/MainActivity.java
@@ -0,0 +1,13 @@
+package flutter.dreamwalker.com.flutter_notebook_second_story;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
