52e9eeb4ca125f2bd5d2517d29fc70085a22e32a
Dreamwalker <aristojeff@gmail.com>
2019-07-14 19:35:24 +0900

    Adds second note

---
+++ lib/interview_app/main_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class InterviewApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SchedulesPage(),
+    );
+  }
+}
+
+class SchedulesPage extends StatefulWidget {
+  @override
+  _SchedulesPageState createState() => _SchedulesPageState();
+}
+
+class _SchedulesPageState extends State<SchedulesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/food_delivery_app/main_page.dart';
+import 'package:flutter_notebook_second_story/interview_app/main_page.dart';
 
-void main() => runApp(FoodDeliveryApp());
+void main() => runApp(InterviewApp());
 
 
