27f1e526c17c322bac8363645ee5e64de9db4f43
Dreamwalker <aristojeff@gmail.com>
2019-07-17 22:12:42 +0900

    Update main_page.dart

--- lib/audio_book_app/main_page.dart
+++ lib/audio_book_app/main_page.dart
@@ -21,13 +21,152 @@ class _MainPageState extends State<MainPage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 32,),
+            SizedBox(
+              height: 48,
+            ),
             Container(
+              padding: EdgeInsets.only(left: 24),
               height: 64,
-              decoration: BoxDecoration(
-                color: Colors.red
+              decoration: BoxDecoration(),
+              child: Row(
+                children: <Widget>[
+                  Material(
+                    child: RichText(
+                        text: TextSpan(children: [
+                      TextSpan(
+                          text: "Au",
+                          style: TextStyle(
+                              color: Colors.deepPurpleAccent,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text: "Book",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold)),
+                    ])),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    icon: Icon(Icons.apps),
+                    color: Colors.orange,
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    color: Colors.black,
+                    onPressed: () {},
+                  ),
+                ],
               ),
-              child: Placeholder(),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Container(
+              height: 260,
+              decoration: BoxDecoration(color: Colors.deepPurpleAccent),
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 16,
+                    left: 0,
+                    bottom: 16,
+                    child:Transform.translate(
+                      offset: Offset(-120, 0),
+                      child: Transform.rotate(
+                        child: Container(
+                          height: 240,
+                          width: 240,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xffe26564)
+                          ),
+                          child: Center(
+                            child: Text("DESIGN & ART", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16
+                            ),),
+                          ),
+                        ), angle: 2.6,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 24,
+                    top: 0,
+                    bottom: 0,
+                    right: 24,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(17),
+                        color: Colors.deepPurple
+                      ),
+                    ),
+
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 24),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "New Arrivals",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 24),
+              height: 240,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.only(top: 8, bottom: 8, right: 8),
+                    decoration: BoxDecoration(border: Border.all()),
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(border: Border.all()),
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(border: Border.all()),
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(border: Border.all()),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16,),
+            Padding(
+              padding: const EdgeInsets.only(left: 24),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Best Sellers",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+              ),
             ),
 
           ],
@@ -36,7 +175,6 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
-
 
 
 
