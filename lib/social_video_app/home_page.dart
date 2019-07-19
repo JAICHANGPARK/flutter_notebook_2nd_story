58527d0d8937cd4739b65d95d60149c4210a58c7
Dreamwalker <aristojeff@gmail.com>
2019-07-20 00:01:54 +0900

    Update home_page.dart

--- lib/social_video_app/home_page.dart
+++ lib/social_video_app/home_page.dart
@@ -167,23 +167,18 @@ class _HomePageState extends State<HomePage> {
               future: _initializeVideoPlayerFuture,
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.done) {
-                  // If the VideoPlayerController has finished initialization, use
-                  // the data it provides to limit the aspect ratio of the VideoPlayer.
                   return ListView(
                     children: <Widget>[
                       Container(
                         height: 180,
                         child: AspectRatio(
                           aspectRatio: _controller.value.aspectRatio,
-                          // Use the VideoPlayer widget to display the video.
                           child: VideoPlayer(_controller),
                         ),
                       )
                     ],
                   );
                 } else {
-                  // If the VideoPlayerController is still initializing, show a
-                  // loading spinner.
                   return Center(child: CircularProgressIndicator());
                 }
               },
