a791514d17858ff698dc45df27550019833e5d2e
Dreamwalker <aristojeff@gmail.com>
2019-07-19 23:32:33 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/audio_book_app/main_page.dart';
-import 'package:flutter_notebook_second_story/love_reading_app/main_page.dart';
 
-void main() => runApp(AudioBookApp());
+import 'package:flutter_notebook_second_story/social_video_app/home_page.dart';
+
+void main() => runApp(SocialVideoApp());
 
 
