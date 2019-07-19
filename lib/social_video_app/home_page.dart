6e5c9f6af73342624a90f33ab8c6fd242eefe6e6
Dreamwalker <aristojeff@gmail.com>
2019-07-19 23:41:18 +0900

    Update home_page.dart

--- lib/social_video_app/home_page.dart
+++ lib/social_video_app/home_page.dart
@@ -21,38 +21,51 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: <Widget>[
           Container(
-            decoration: BoxDecoration(
-
-            ),
+            decoration: BoxDecoration(),
             height: 180,
-           child: Placeholder(),
+            child: Row(
+
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: Icon(Icons.menu),
+                      iconSize: 38,
+                      color: Colors.blue,
+                      onPressed: () {}),
+                ),
+                Expanded(
+                  flex: 8,
+
+                    child: Container(
+                      padding: EdgeInsets.only(top: 72),
+                      color: Color(0xfff6f5fa),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("My Favourites",style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w500,
+                          ),),
+                          SizedBox(height: 24,)
+                        ],
+                      ),
+                    ),
+
+                )
+              ],
+            ),
           ),
           Container(
-            height: MediaQuery.of(context).size.height-180,
+            height: MediaQuery.of(context).size.height - 180,
             child: Placeholder(),
-
           )
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
