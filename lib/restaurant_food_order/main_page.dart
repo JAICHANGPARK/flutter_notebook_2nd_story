48ca22ffb278d5bda73a26afecf00beee1fa7a7a
Dreamwalker <aristojeff@gmail.com>
2019-07-24 21:21:55 +0900

    Update main_page.dart

--- lib/restaurant_food_order/main_page.dart
+++ lib/restaurant_food_order/main_page.dart
@@ -16,7 +16,74 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search),
+          color: Colors.grey,
+          onPressed: (){},),
+          IconButton(icon: Icon(Icons.notifications),
+            color: Colors.grey,
+            onPressed: (){},)
+        ],
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Text("Explore our\nDelicious Offers",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),),
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
