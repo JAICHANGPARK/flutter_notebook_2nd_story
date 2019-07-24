03cb3a2d32b5042ead2cb3e34256606709bb472b
Dreamwalker <aristojeff@gmail.com>
2019-07-24 21:35:11 +0900

    Update main_page.dart

--- lib/restaurant_food_order/main_page.dart
+++ lib/restaurant_food_order/main_page.dart
@@ -8,6 +8,7 @@ class RestaurantFoodOrderApp extends StatelessWidget {
     );
   }
 }
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -22,12 +23,16 @@ class _MainPageState extends State<MainPage> {
         backgroundColor: Colors.white,
         elevation: 0,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search),
-          color: Colors.grey,
-          onPressed: (){},),
-          IconButton(icon: Icon(Icons.notifications),
+          IconButton(
+            icon: Icon(Icons.search),
             color: Colors.grey,
-            onPressed: (){},)
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.notifications),
+            color: Colors.grey,
+            onPressed: () {},
+          )
         ],
       ),
       body: SingleChildScrollView(
@@ -38,20 +43,22 @@ class _MainPageState extends State<MainPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
-                child: Text("Explore our\nDelicious Offers",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.0
-                ),),
+                child: Text(
+                  "Explore our\nDelicious Offers",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0),
+                ),
               ),
-              SizedBox(height: 16,),
+              SizedBox(
+                height: 16,
+              ),
               Container(
                 height: 280,
                 child: ListView(
                   scrollDirection: Axis.horizontal,
-
                   children: <Widget>[
                     Container(
                       margin: EdgeInsets.only(right: 16),
@@ -64,12 +71,12 @@ class _MainPageState extends State<MainPage> {
                             child: Container(
                               margin: EdgeInsets.only(bottom: 8),
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                color: Colors.redAccent,
-                                image: DecorationImage(image: 
-                                NetworkImage("https://cdn.pixabay.com/photo/2018/08/30/20/47/gugelhupf-3643259_960_720.jpg"),
-                                fit: BoxFit.cover)
-                              ),
+                                  borderRadius: BorderRadius.circular(16),
+                                  color: Colors.redAccent,
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          "https://cdn.pixabay.com/photo/2018/08/30/20/47/gugelhupf-3643259_960_720.jpg"),
+                                      fit: BoxFit.cover)),
                             ),
                           ),
                           Expanded(
@@ -79,10 +86,11 @@ class _MainPageState extends State<MainPage> {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
-                                  Text("Almost Famous",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                  ),),
+                                  Text(
+                                    "Almost Famous",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
                                   Text("Chinese, South indian"),
                                 ],
                               ),
@@ -104,10 +112,10 @@ class _MainPageState extends State<MainPage> {
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(16),
                                   color: Colors.redAccent,
-                                  image: DecorationImage(image:
-                                  NetworkImage("https://cdn.pixabay.com/photo/2016/02/29/00/19/cake-1227842_960_720.jpg"),
-                                      fit: BoxFit.cover)
-                              ),
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          "https://cdn.pixabay.com/photo/2016/02/29/00/19/cake-1227842_960_720.jpg"),
+                                      fit: BoxFit.cover)),
                             ),
                           ),
                           Expanded(
@@ -117,10 +125,11 @@ class _MainPageState extends State<MainPage> {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
-                                  Text("Almost Famous",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold
-                                    ),),
+                                  Text(
+                                    "Almost Famous",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
                                   Text("Chinese, South indian"),
                                 ],
                               ),
@@ -131,8 +140,51 @@ class _MainPageState extends State<MainPage> {
                     ),
                   ],
                 ),
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Divider(
+                  color: Colors.grey,
+                  indent: 4,
+                  endIndent: 4,
+                  height: 2,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 16, top: 16),
+                child: Row(
+                  children: <Widget>[
+                    Text("Near Me (15)"),
+                    Spacer(),
+                    Icon(Icons.tune),
+                    Text("Filter")
+                  ],
+                ),
+              ),
+              SizedBox(height: 8,),
+              ListView(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                children: <Widget>[
+                  Container(
+                    height: 120,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            image: DecorationImage(image: NetworkImage("https://cdn.pixabay.com/photo/2018/09/09/21/58/dounat-3665638_960_720.jpg"),
+                            fit: BoxFit.cover)
+                          ),
+                        ),
+                        SizedBox(width: 16,),
 
+                      ],
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
@@ -140,16 +192,6 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
