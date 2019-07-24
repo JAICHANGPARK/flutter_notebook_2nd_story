42ae17bfe0cfca33c1d9a425b267e941e666f3e7
Dreamwalker <aristojeff@gmail.com>
2019-07-24 21:48:36 +0900

    Update main_page.dart

--- lib/restaurant_food_order/main_page.dart
+++ lib/restaurant_food_order/main_page.dart
@@ -14,7 +14,19 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with
+SingleTickerProviderStateMixin{
+  AnimationController _animationController;
+  Animation<double> _buttonAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(vsync: this, duration: Duration(
+      seconds: 1
+    ));
+    _buttonAnimation = Tween<double>(begin: 0, end: 1).animate(_animationController);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -161,25 +173,277 @@ class _MainPageState extends State<MainPage> {
                   ],
                 ),
               ),
-              SizedBox(height: 8,),
+              SizedBox(
+                height: 8,
+              ),
               ListView(
                 scrollDirection: Axis.vertical,
                 shrinkWrap: true,
                 children: <Widget>[
                   Container(
-                    height: 120,
+                    height: 110,
                     child: Row(
                       children: <Widget>[
                         Container(
                           width: 100,
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            image: DecorationImage(image: NetworkImage("https://cdn.pixabay.com/photo/2018/09/09/21/58/dounat-3665638_960_720.jpg"),
-                            fit: BoxFit.cover)
-                          ),
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://cdn.pixabay.com/photo/2018/09/09/21/58/dounat-3665638_960_720.jpg"),
+                                  fit: BoxFit.cover)),
                         ),
-                        SizedBox(width: 16,),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Container(
+                          width: 240,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Royal Food Count",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  Row(
+                                    children: <Widget>[
+                                      CircleAvatar(
+                                        radius: 4,
+                                        backgroundColor: Colors.greenAccent,
+                                      ),
+                                      SizedBox(width: 8,),
+                                      Text(
+                                        "Open",
+                                        style: TextStyle(
+                                            color: Colors.greenAccent,
+                                            fontSize: 12),
+                                      )
+                                    ],
+                                  ),
 
+                                ],
+                              ),
+                              Text("Chinese, South indian"),
+                              SizedBox(height: 8,),
+                              Row(
+                                children: <Widget>[
+                                  Icon(Icons.location_on, size: 12,
+                                  color: Colors.grey,),
+                                  Text("100-102 High St, Manchester, UK",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),)
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Container(
+                    height: 110,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://cdn.pixabay.com/photo/2018/09/09/21/58/dounat-3665638_960_720.jpg"),
+                                  fit: BoxFit.cover)),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Container(
+                          width: 240,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Royal Food Count",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  Row(
+                                    children: <Widget>[
+                                      CircleAvatar(
+                                        radius: 4,
+                                        backgroundColor: Colors.greenAccent,
+                                      ),
+                                      SizedBox(width: 8,),
+                                      Text(
+                                        "Open",
+                                        style: TextStyle(
+                                            color: Colors.greenAccent,
+                                            fontSize: 12),
+                                      )
+                                    ],
+                                  ),
+
+                                ],
+                              ),
+                              Text("Chinese, South indian"),
+                              SizedBox(height: 8,),
+                              Row(
+                                children: <Widget>[
+                                  Icon(Icons.location_on, size: 12,
+                                    color: Colors.grey,),
+                                  Text("100-102 High St, Manchester, UK",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),)
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Container(
+                    height: 110,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://cdn.pixabay.com/photo/2018/09/09/21/58/dounat-3665638_960_720.jpg"),
+                                  fit: BoxFit.cover)),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Container(
+                          width: 240,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Royal Food Count",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  Row(
+                                    children: <Widget>[
+                                      CircleAvatar(
+                                        radius: 4,
+                                        backgroundColor: Colors.greenAccent,
+                                      ),
+                                      SizedBox(width: 8,),
+                                      Text(
+                                        "Open",
+                                        style: TextStyle(
+                                            color: Colors.greenAccent,
+                                            fontSize: 12),
+                                      )
+                                    ],
+                                  ),
+
+                                ],
+                              ),
+                              Text("Chinese, South indian"),
+                              SizedBox(height: 8,),
+                              Row(
+                                children: <Widget>[
+                                  Icon(Icons.location_on, size: 12,
+                                    color: Colors.grey,),
+                                  Text("100-102 High St, Manchester, UK",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),)
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Container(
+                    height: 110,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://cdn.pixabay.com/photo/2018/09/09/21/58/dounat-3665638_960_720.jpg"),
+                                  fit: BoxFit.cover)),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Container(
+                          width: 240,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Royal Food Count",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  Row(
+                                    children: <Widget>[
+                                      CircleAvatar(
+                                        radius: 4,
+                                        backgroundColor: Colors.greenAccent,
+                                      ),
+                                      SizedBox(width: 8,),
+                                      Text(
+                                        "Open",
+                                        style: TextStyle(
+                                            color: Colors.greenAccent,
+                                            fontSize: 12),
+                                      )
+                                    ],
+                                  ),
+
+                                ],
+                              ),
+                              Text("Chinese, South indian"),
+                              SizedBox(height: 8,),
+                              Row(
+                                children: <Widget>[
+                                  Icon(Icons.location_on, size: 12,
+                                    color: Colors.grey,),
+                                  Text("100-102 High St, Manchester, UK",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),)
+                                ],
+                              )
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   )
@@ -188,17 +452,18 @@ class _MainPageState extends State<MainPage> {
             ],
           ),
         ),
+
       ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        if(_animationController.status == AnimationStatus.completed){
+          _animationController.reverse();
+        }else  _animationController.forward();
+
+      },
+      child: AnimatedIcon(icon: AnimatedIcons.add_event, progress: _buttonAnimation),),
     );
   }
 }
-
-
-
-
-
-
-
 
 
 
