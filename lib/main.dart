332f5ea7f7999bf9a3e723c0b144c52bba3e24cc
Dreamwalker <aristojeff@gmail.com>
2019-07-26 22:41:44 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/delivery_app/main_page.dart';
-import 'package:flutter_notebook_second_story/restaurant_food_order/main_page.dart';
 
-void main() => runApp(DeliveryApp());
+import 'package:flutter_notebook_second_story/smart_home_app/main_page.dart';
+
+void main() => runApp(SmartHomeApp());
 
 
--- lib/smart_home_app/main_page.dart
+++ lib/smart_home_app/main_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const Color appPurpleColor = Color(0xff3e399b);
+const Color appBrownColor = Color(0xffd7964a);
+
 class SmartHomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,36 +17,100 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+  double initHeight = 80;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(vsync: this);
+    animation = Tween<double>(begin: 80, end: 480).animate(animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              width: 100,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(color: appPurpleColor),
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    left: 24,
+                    top: 24,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                      ),
+                      iconSize: 38,
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 120,
+            bottom: 0,
+            child: Transform.translate(
+              offset: Offset(-400, -100),
+              child: Transform.rotate(
+                angle: 1.56,
+                child: Container(
+                  child: Text(
+                    "Livingroom",
+                    style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 10,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 48),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: InkWell(
+              onTap: () {
+                animationController.forward();
+              },
+              child: AnimatedContainer(
+                width: 100,
+                height: animation.value,
+                decoration: BoxDecoration(color: appBrownColor),
+                child: Center(
+                  child: Text(
+                    "ADD TO",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                duration: Duration(seconds: 1),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
