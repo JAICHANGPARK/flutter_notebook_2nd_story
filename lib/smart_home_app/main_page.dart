44ee40be1cd4a9ff6dfc162db6f425c9eb29572f
Dreamwalker <aristojeff@gmail.com>
2019-07-26 22:21:21 +0900

    Create main_page.dart

---
+++ lib/smart_home_app/main_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SmartHomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
