8f18cef2b00074c7d695a055fc71d1dd7fef92b2
Dreamwalker <aristojeff@gmail.com>
2019-07-29 23:15:53 +0900

    Update main_page.dart

--- lib/food_delivery_app_02/main_page.dart
+++ lib/food_delivery_app_02/main_page.dart
@@ -61,7 +61,37 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            top: 0,
+            right: 0,
+            child: Container(
+              height: 340,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: NetworkImage("https://cdn.pixabay.com/photo/2015/12/08/00/32/steak-1081819_960_720.jpg"),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.darken))
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 270,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(64)
+                )
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
