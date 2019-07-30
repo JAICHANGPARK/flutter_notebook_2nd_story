65d0997e9a443269bd410b426e960613f55ccdc8
Dreamwalker <aristojeff@gmail.com>
2019-07-30 21:48:07 +0900

    Update main_page.dart

--- lib/vinyl_app/main_page.dart
+++ lib/vinyl_app/main_page.dart
@@ -19,7 +19,29 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.deepPurple,
+      body: Column(
+        children: <Widget>[
+          SafeArea(
+            top: true,
+            left: true,
+            right: true,
+            child: Container(
+              height: 48,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(32)
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.search)
+                ),
+              )
+            ),
+          )
+        ],
+      ),
     );
   }
 }
