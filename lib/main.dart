2f97e624b35ef806f6b4bc53d0f7183710e549cc
Dreamwalker <aristojeff@gmail.com>
2019-08-05 20:54:45 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/travel_app_animation/main_page.dart';
+import 'package:flutter_notebook_second_story/fitness_app/main_page.dart';
 
-void main() => runApp(TravelAppWithAnimation());
+void main() => runApp(FitnessApp());
 
 
