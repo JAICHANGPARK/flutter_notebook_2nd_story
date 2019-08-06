49d9d783804fc29ec509611049e43a7377210b47
Dreamwalker <aristojeff@gmail.com>
2019-08-06 21:16:22 +0900

    Adds

--- lib/furniture_app/main_page.dart
+++ lib/furniture_app/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FurnitureApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,34 +8,67 @@ class FurnitureApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> _buildBackgroundWidget(BuildContext context) {
+    return [
+      Positioned(
+        left: 0,
+        right: 0,
+        top: 0,
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2,
+          decoration: BoxDecoration(color: Color(0xffffcc1e)),
+        ),
+      ),
+      Positioned(
+        bottom: 0,
+        left: 0,
+        right: 0,
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2,
+          decoration: BoxDecoration(
+            color: Color(0xfffcf8e7),
+          ),
+        ),
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    var deviceMedia = MediaQuery.of(context);
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          Positioned(
-            left: 0,
-              right: 0,
-            top: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xffffcc1e)
-              ),
-            ),
+          ..._buildBackgroundWidget(context).map((m)=>m),
 
-          )
         ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
