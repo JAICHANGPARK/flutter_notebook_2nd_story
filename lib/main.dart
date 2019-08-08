8a37f29b0c751767a873f1ddd4a151da35ed1557
Dreamwalker <aristojeff@gmail.com>
2019-08-08 21:37:38 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/plant_app/main_pae.dart';
+import 'package:flutter_notebook_second_story/pharmacy_app/main_page.dart';
 
-void main() => runApp(PlantsApp());
+void main() => runApp(PharmacyApp());
 
 
---
+++ lib/pharmacy_app/main_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class PharmacyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MapPage(),
+    );
+  }
+}
+
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
