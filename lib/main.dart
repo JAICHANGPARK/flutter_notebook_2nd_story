52bdd98c3d6137bd5212c869a0a8a8a940b3892b
Dreamwalker <aristojeff@gmail.com>
2019-08-09 23:07:30 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/pharmacy_app/main_page.dart';
-
-void main() => runApp(PharmacyApp());
+import 'package:flutter_notebook_second_story/meeting_roombooking_app/main_page.dart';
+void main() => runApp(MeetingRoomBookingApp());
 
 
--- lib/meeting_roombooking_app/main_page.dart
+++ lib/meeting_roombooking_app/main_page.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
 
+class MeetingRoomBookingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BookingPage(),
+    );
+  }
+}
+class BookingPage extends StatefulWidget {
+  @override
+  _BookingPageState createState() => _BookingPageState();
+}
+
+class _BookingPageState extends State<BookingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
