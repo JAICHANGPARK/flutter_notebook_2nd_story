a47f17a39219be7e66e34cc51695a15f07aa0eeb
Dreamwalker <aristojeff@gmail.com>
2019-08-10 16:52:32 +0900

    Adds

---
+++ lib/bubble_tea_delivery/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BubbleTeaDelivery extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
