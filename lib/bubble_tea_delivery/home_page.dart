dafab4352c2fa97e75dd88af39a970ea565112ea
Dreamwalker <aristojeff@gmail.com>
2019-08-10 17:01:07 +0900

    Update home_page.dart

--- lib/bubble_tea_delivery/home_page.dart
+++ lib/bubble_tea_delivery/home_page.dart
@@ -5,6 +5,7 @@ class BubbleTeaDelivery extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -24,13 +25,111 @@ class _HomePageState extends State<HomePage> {
             left: 16,
             top: 24,
             right: 16,
-            child: Row(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("DreamWalker Shop"),
+                        Text("Roppongi, Tokyo"),
+                      ],
+                    ),
+                    Spacer(),
+
+                    Column(
+                      children: <Widget>[
+                        Text("20"),
+                        Text('mins')
+                      ],
+                    ),
+                    SizedBox(width: 16,),
+                    Container(
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.blueGrey,
+                          width: 5
+                        )
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child:Container(
+                              height: 16,
+                              width: 16,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.red
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+
+                  ],
+                ),
+                Divider(height: 16,),
+                SizedBox(height: 16,),
+                Text("NEW DRINKS",style: TextStyle(
+                  fontSize: 12
+                ),),
+              ],
+            ),
+          ),
+          Positioned(
+            left: 16,
+            top: 140,
+            right: 0,
+            child: Container(
+              height: 280,
+              decoration: BoxDecoration(
+                color: Colors.red
+              ),
+              child: ListView(),
+            ),
           )
         ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
