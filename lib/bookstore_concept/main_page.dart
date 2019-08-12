5726a7968956a792b471230e8a4780b3bb3e8d7a
Dreamwalker <aristojeff@gmail.com>
2019-08-12 21:13:12 +0900

    Update main_page.dart

--- lib/bookstore_concept/main_page.dart
+++ lib/bookstore_concept/main_page.dart
@@ -16,10 +16,75 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  bool uiSwitcher = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 32,),
+          //TODO Implement Appbar
+          Container(
+            height: 48,
+            width: MediaQuery.of(context).size.width,
+              child: Placeholder(),
+          ),
+          //TODO Implement Flexible Top UI
+          Expanded(
+            flex: uiSwitcher ? 1 : 4,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: uiSwitcher ? 6: 1,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: uiSwitcher ? 1: 4,
+            child: Placeholder(),
+          )
 
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            setState(() {
+              uiSwitcher = !uiSwitcher;
+            });
+
+      }),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
