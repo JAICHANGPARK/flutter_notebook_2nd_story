d99bcf48148f5c2d8bfd38b1b73d0a9f217d97ac
Dreamwalker <aristojeff@gmail.com>
2019-08-12 21:05:58 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_second_story/bookstore_concept/main_page.dart';
 import 'package:flutter_notebook_second_story/bubble_tea_delivery/home_page.dart';
 
-void main() => runApp(BubbleTeaDelivery());
+void main() => runApp(BookstoreConcept());
 
 
