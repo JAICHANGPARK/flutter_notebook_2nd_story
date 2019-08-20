34a52ab97756076089422f480a6d5b6fe63862c8
Dreamwalker <aristojeff@gmail.com>
2019-08-20 20:55:32 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/hair_salons/main_page.dart';
-import 'package:flutter_notebook_second_story/ticket_app/main_page.dart';
-import 'package:flutter_notebook_second_story/traveler/main_page.dart';
+import 'package:flutter_notebook_second_story/music_streaming_app/main_page.dart';
 
-void main() => runApp(TravelerApp());
+
+void main() => runApp(MusicStreamingApp());
 
 
