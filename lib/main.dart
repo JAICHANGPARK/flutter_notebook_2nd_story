713a051333da4b422ba3af51f9d8afcac4c704c2
Dreamwalker <aristojeff@gmail.com>
2019-08-22 22:33:18 +0900

    Update money management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_second_story/money_management_app/main_page.dart';
 import 'package:flutter_notebook_second_story/music_streaming_app/main_page.dart';
 
 
-void main() => runApp(MusicStreamingApp());
+void main() => runApp(MoneyManagementApp());
 
 
--- lib/money_management_app/main_page.dart
+++ lib/money_management_app/main_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_second_story/note_utils/note_image.dart';
 
-class MoneyMangementApp extends StatelessWidget {
+class MoneyManagementApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MainPage(),
-    )
+    );
   }
 }
 
@@ -21,16 +22,66 @@ class _MainPageState extends State<MainPage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
+            SafeArea(
+              top: true,
+              left: true,
+              child: SizedBox(
+                height: 48,
+              ),
+            ),
+            Container(
+              height: 80,
+              padding: EdgeInsets.only(left: 16, right: 16),
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Welcome back,", style: TextStyle(color:
+                      Colors.grey,
+                      ),),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Dreamwalker",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Container(
+                    height: 52,
+                    width: 52,
+                    decoration: BoxDecoration(color: Colors.blue,
+                    image: DecorationImage(
+                      image: NetworkImage(dreamwalkerImg)
+                    )),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 120,
+              margin: EdgeInsets.all(16),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.blueAccent
 
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
-
 
 
 
