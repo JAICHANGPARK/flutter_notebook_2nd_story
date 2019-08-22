9ee272f6efaf5ad3cb2afd81691d12b7fbbb9029
Dreamwalker <aristojeff@gmail.com>
2019-08-22 22:25:53 +0900

    Create main_page.dart

---
+++ lib/money_management_app/main_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MoneyMangementApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    )
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
