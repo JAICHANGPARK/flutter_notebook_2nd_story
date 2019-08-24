0ad835af6beb7185ed5125c5e477fff6a4af1a9f
Dreamwalker <aristojeff@gmail.com>
2019-08-24 18:01:04 +0900

    Update main_page.dart

--- lib/groceries_shopping_app/main_page.dart
+++ lib/groceries_shopping_app/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class Food{
+class Food {
   final String imgPath;
   final String price;
   final String title;
@@ -11,53 +12,53 @@ class Food{
 
 List<Food> foodItems = [
   Food(
-    imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
-    price: "\$1.99",
-    title: "Lays's Cheddar & Sour Cream",
-    subtitle: "500g"
-  ),
-  Food(
-      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
-      price: "\$1.99",
-      title: "Lays's Honey Barbecue",
-      subtitle: "500g"
-  ),
-  Food(
-      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
+      imgPath:
+          "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
       price: "\$1.99",
       title: "Lays's Cheddar & Sour Cream",
-      subtitle: "500g"
-  ),
+      subtitle: "500g"),
   Food(
-      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
+      imgPath:
+          "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
       price: "\$1.99",
       title: "Lays's Honey Barbecue",
-      subtitle: "500g"
-  ),
+      subtitle: "500g"),
   Food(
-      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
+      imgPath:
+          "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
       price: "\$1.99",
       title: "Lays's Cheddar & Sour Cream",
-      subtitle: "500g"
-  ),
+      subtitle: "500g"),
   Food(
-      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
+      imgPath:
+          "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
       price: "\$1.99",
       title: "Lays's Honey Barbecue",
-      subtitle: "500g"
-  ),
+      subtitle: "500g"),
   Food(
-      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
+      imgPath:
+          "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
       price: "\$1.99",
       title: "Lays's Cheddar & Sour Cream",
-      subtitle: "500g"
-  ),
+      subtitle: "500g"),
   Food(
-      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
+      imgPath:
+          "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
       price: "\$1.99",
       title: "Lays's Honey Barbecue",
-      subtitle: "500g"
-  ),
+      subtitle: "500g"),
+  Food(
+      imgPath:
+          "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
+      price: "\$1.99",
+      title: "Lays's Cheddar & Sour Cream",
+      subtitle: "500g"),
+  Food(
+      imgPath:
+          "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
+      price: "\$1.99",
+      title: "Lays's Honey Barbecue",
+      subtitle: "500g"),
 ];
 
 class GroceriesShoppingApp extends StatelessWidget {
@@ -75,32 +76,79 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double containerHeight = MediaQuery.of(context).size.height - 120;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            AnimatedContainer(
+              padding: EdgeInsets.all(16),
+                height: containerHeight,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(color: Colors.white,
+                borderRadius: BorderRadius.circular(16)),
+                duration: Duration(microseconds: 700),
+                child: StaggeredGridView.countBuilder(
+                  itemCount: foodItems.length,
+                  crossAxisCount: 2,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.only(left: 16, right: 16),
+                      margin: EdgeInsets.all(8),
+                      decoration: BoxDecoration(color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.15),
+                          blurRadius: 2,
+                          spreadRadius: 1
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(16),),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.network(foodItems[index].imgPath,width: 160, height: 100,alignment:
+                            Alignment.center,),
+                          SizedBox(height: 16,),
+                          Text(foodItems[index].price,
+                            style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          ),),
+                          SizedBox(height: 16,),
+                          Text(foodItems[index].title,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+
+                            ),),
+                          SizedBox(height: 4,),
+                          Text(foodItems[index].subtitle,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10,
+
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  staggeredTileBuilder: (index) {
+                    return StaggeredTile.count(1, index.isEven ? 1.6 : 1.4);
+                  },
+//                  mainAxisSpacing: 8.0,
+//                  crossAxisSpacing: 8.0,
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
