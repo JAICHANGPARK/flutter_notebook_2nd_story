1ffe9ba8e7cb29ba293708a93c75a979420bec8c
Dreamwalker <aristojeff@gmail.com>
2019-08-24 17:45:08 +0900

    Adds

--- lib/groceries_shopping_app/main_page.dart
+++ lib/groceries_shopping_app/main_page.dart
@@ -1,5 +1,65 @@
 import 'package:flutter/material.dart';
 
+class Food{
+  final String imgPath;
+  final String price;
+  final String title;
+  final String subtitle;
+
+  Food({this.imgPath, this.price, this.title, this.subtitle});
+}
+
+List<Food> foodItems = [
+  Food(
+    imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
+    price: "\$1.99",
+    title: "Lays's Cheddar & Sour Cream",
+    subtitle: "500g"
+  ),
+  Food(
+      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
+      price: "\$1.99",
+      title: "Lays's Honey Barbecue",
+      subtitle: "500g"
+  ),
+  Food(
+      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
+      price: "\$1.99",
+      title: "Lays's Cheddar & Sour Cream",
+      subtitle: "500g"
+  ),
+  Food(
+      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
+      price: "\$1.99",
+      title: "Lays's Honey Barbecue",
+      subtitle: "500g"
+  ),
+  Food(
+      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
+      price: "\$1.99",
+      title: "Lays's Cheddar & Sour Cream",
+      subtitle: "500g"
+  ),
+  Food(
+      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
+      price: "\$1.99",
+      title: "Lays's Honey Barbecue",
+      subtitle: "500g"
+  ),
+  Food(
+      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-chips-packpotato-tasty-pack-lays-chips-snack-taste-product-941524636381ybdhu.png",
+      price: "\$1.99",
+      title: "Lays's Cheddar & Sour Cream",
+      subtitle: "500g"
+  ),
+  Food(
+      imgPath: "https://purepng.com/public/uploads/large/purepng.com-lays-potato-chips-packfood-potato-tasty-pack-lays-chips-taste-product-snaks-941524625368gdogh.png",
+      price: "\$1.99",
+      title: "Lays's Honey Barbecue",
+      subtitle: "500g"
+  ),
+];
+
 class GroceriesShoppingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,7 +78,29 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
