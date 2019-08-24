11060d75dfd33cdbaaef99cb9efee71835ce53de
Dreamwalker <aristojeff@gmail.com>
2019-08-24 17:37:52 +0900

    Adds food app?

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/money_management_app/main_page.dart';
-import 'package:flutter_notebook_second_story/music_streaming_app/main_page.dart';
+import 'package:flutter_notebook_second_story/groceries_shopping_app/main_page.dart';
 
-
-void main() => runApp(MoneyManagementApp());
+void main() => runApp(GroceriesShoppingApp());
 
 
