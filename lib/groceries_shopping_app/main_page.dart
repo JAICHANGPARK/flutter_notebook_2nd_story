fdc1334adee947ccd58bff9dc43837c0e49df506
Dreamwalker <aristojeff@gmail.com>
2019-08-25 17:18:07 +0900

    Update main_page.dart

--- lib/groceries_shopping_app/main_page.dart
+++ lib/groceries_shopping_app/main_page.dart
@@ -83,6 +83,7 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
     bottomRight: Radius.circular(32),
   );
   bool selectedTrigger = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,100 +92,127 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
         child: Column(
           children: <Widget>[
             AnimatedContainer(
-              curve: Curves.easeIn,
+                curve: Curves.easeIn,
                 padding: EdgeInsets.all(16),
                 height: containerHeight,
                 decoration: BoxDecoration(
                     color: Colors.white, borderRadius: _borderRadiusGeometry),
                 duration: Duration(milliseconds: 1000),
-                child: StaggeredGridView.countBuilder(
-                  itemCount: foodItems.length,
-                  crossAxisCount: 2,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        print("Item Clicked");
-                        setState(() {
-                          if(!selectedTrigger){
-                            containerHeight = MediaQuery.of(context).size.height;
-                            _borderRadiusGeometry = BorderRadius.circular(0);
-                            selectedTrigger = !selectedTrigger;
-
-                          }else{
-                             containerHeight = 657.4545454545455;
-                             _borderRadiusGeometry = BorderRadius.only(
-                              bottomLeft: Radius.circular(32),
-                              bottomRight: Radius.circular(32),
-                            );
-                             selectedTrigger = !selectedTrigger;
-                          }
-
-                        });
-                      },
-                      child: Container(
-                        padding: EdgeInsets.only(left: 16, right: 16),
-                        margin: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black.withOpacity(0.15),
-                                blurRadius: 2,
-                                spreadRadius: 1)
-                          ],
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Image.network(
-                              foodItems[index].imgPath,
-                              width: 160,
-                              height: 100,
-                              alignment: Alignment.center,
-                            ),
-                            SizedBox(
-                              height: 16,
-                            ),
-                            Text(
-                              foodItems[index].price,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: 16,
-                            ),
-                            Text(
-                              foodItems[index].title,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text(
-                              foodItems[index].subtitle,
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ],
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 64,
+                        child: AppBar(
+                          backgroundColor: Colors.white,
+                          title: Text(
+                            "Photato Chips",
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                          elevation: 0,
+                          iconTheme: IconThemeData(color: Colors.black),
+                          leading: Icon(
+                            Icons.arrow_back_ios,
+                            size: 16,
+                          ),
+                          actions: <Widget>[Icon(Icons.tune)],
                         ),
                       ),
-                    );
-                  },
-                  staggeredTileBuilder: (index) {
-                    return StaggeredTile.count(1, index.isEven ? 1.6 : 1.4);
-                  },
+                      Container(
+                        height: containerHeight - 100,
+                        child: StaggeredGridView.countBuilder(
+                          itemCount: foodItems.length,
+                          crossAxisCount: 2,
+                          itemBuilder: (context, index) {
+                            return InkWell(
+                              onTap: () {
+                                print("Item Clicked");
+                                setState(() {
+                                  if (!selectedTrigger) {
+                                    containerHeight =
+                                        MediaQuery.of(context).size.height;
+                                    _borderRadiusGeometry =
+                                        BorderRadius.circular(0);
+                                    selectedTrigger = !selectedTrigger;
+                                  } else {
+                                    containerHeight = 657.4545454545455;
+                                    _borderRadiusGeometry = BorderRadius.only(
+                                      bottomLeft: Radius.circular(32),
+                                      bottomRight: Radius.circular(32),
+                                    );
+                                    selectedTrigger = !selectedTrigger;
+                                  }
+                                });
+                              },
+                              child: Container(
+                                padding: EdgeInsets.only(left: 16, right: 16),
+                                margin: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.15),
+                                        blurRadius: 2,
+                                        spreadRadius: 1)
+                                  ],
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Image.network(
+                                      foodItems[index].imgPath,
+                                      width: 160,
+                                      height: 100,
+                                      alignment: Alignment.center,
+                                    ),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    Text(
+                                      foodItems[index].price,
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    Text(
+                                      foodItems[index].title,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 4,
+                                    ),
+                                    Text(
+                                      foodItems[index].subtitle,
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                          staggeredTileBuilder: (index) {
+                            return StaggeredTile.count(
+                                1, index.isEven ? 1.6 : 1.4);
+                          },
 //                  mainAxisSpacing: 8.0,
 //                  crossAxisSpacing: 8.0,
-                ))
+                        ),
+                      )
+                    ],
+                  ),
+                )),
           ],
         ),
       ),
