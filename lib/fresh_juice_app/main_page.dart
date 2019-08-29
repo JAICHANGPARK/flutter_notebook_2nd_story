9ac32309e0cdb45c5ff13a7cb1caa02802988e67
Dreamwalker <aristojeff@gmail.com>
2019-08-29 21:08:59 +0900

    A

--- lib/fresh_juice_app/main_page.dart
+++ lib/fresh_juice_app/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FreshJuiceApp extends StatelessWidget {
@@ -28,8 +29,133 @@ class _MainPageState extends State<MainPage> {
             child: Container(
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: NetworkImage("https://cdn.pixabay.com/photo/2016/05/08/21/24/strawberry-1379986_960_720.jpg")
-                )
+                    image: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2016/05/08/21/24/strawberry-1379986_960_720.jpg"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 8,
+            right: 8,
+            bottom: 8,
+            child: Container(
+              height: 280,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    top: 24,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(24)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Container(
+                              height: 4,
+                              width: 64,
+                              decoration:
+                                  BoxDecoration(color: Colors.grey[300]),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  "Pressed Juice",
+                                  style: TextStyle(
+                                      fontSize: 28, color: Colors.blueGrey),
+                                ),
+                                Text(
+                                  "\$6.00",
+                                  style: TextStyle(
+                                      color: Colors.pinkAccent,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    height: 28,
+                                    margin: EdgeInsets.only(right: 16),
+                                    decoration: BoxDecoration(
+                                      color: Colors.deepPurpleAccent,
+                                      borderRadius: BorderRadius.circular(4)
+                                    ),
+                                    child: Center(
+                                      child: Text("Description",
+                                      style: TextStyle(
+                                        color: Colors.white
+                                      ),),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    margin: EdgeInsets.only(left: 16),
+                                    child: Text("Ingredients"),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    child: Text("Nutrition Facts"),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 24,
+                    top: 0,
+                    child: Container(
+                      height: 48,
+                      width: 48,
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurpleAccent,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 1,
+                                spreadRadius: 3)
+                          ],
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Center(
+                        child: Icon(
+                          Icons.bookmark_border,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           )
@@ -38,23 +164,3 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/smart_to_do_app/main_page.dart';
+import 'package:flutter_notebook_second_story/fresh_juice_app/main_page.dart';
 
-void main() => runApp(SmartToDoApp());
+void main() => runApp(FreshJuiceApp());
 
 
