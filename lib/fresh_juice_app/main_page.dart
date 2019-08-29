b7655c9ff3d6ae33e194278da5003dd201ffceb6
Dreamwalker <aristojeff@gmail.com>
2019-08-29 20:54:21 +0900

    Create main_page.dart

---
+++ lib/fresh_juice_app/main_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class FreshJuiceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
