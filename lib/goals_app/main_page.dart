b11fe79a3bbb2bbe3f5abbeb4159bbda9176a484
Dreamwalker <aristojeff@gmail.com>
2019-08-31 00:19:23 +0900

    Update main_page.dart

--- lib/goals_app/main_page.dart
+++ lib/goals_app/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_second_story/note_utils/note_image.dart';
 
@@ -86,7 +87,126 @@ class _MainPageState extends State<MainPage> {
                       ],
                     ),
                   ),
-
+                  Container(
+                    height: 240,
+                    margin: EdgeInsets.only(top: 16),
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      children: <Widget>[
+                        Container(
+                          width: 200,
+                          margin: EdgeInsets.only(right: 16),
+                          padding: EdgeInsets.only(
+                          top: 16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: Colors.deepPurple.withOpacity(0.7)
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16),
+                                child: Container(
+                                  height: 38,
+                                  width: 38,
+                                  decoration: BoxDecoration(
+                                    color: Colors.orange,
+                                    borderRadius: BorderRadius.circular(8)
+                                  ),
+                                  child: Stack(
+                                    children: <Widget>[
+                                      Positioned(
+                                        left: -8,
+                                        top: -16,
+                                        child: Container(
+                                          height: 38,
+                                          width: 38,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.white.withOpacity(0.8)
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: -12,
+                                        child: Container(
+                                          height: 38,
+                                          width: 38,
+                                          decoration: BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Colors.white.withOpacity(0.5)
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16),
+                                child: Text("Lose 15 Ibs",style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24
+                                ),),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16),
+                                child: Text("Become even prettier\nand wear that\ngargeous dress!",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  letterSpacing: 1.2,
+                                  color: Colors.white.withOpacity(0.7)
+                                ),),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16, right: 8),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text("Progress", style: TextStyle(
+                                      color: Colors.white,
+                                    ),),
+                                    Text("50%",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16
+                                      ),)
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 0,),
+                                child: Container(
+                                  height: 12,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlueAccent,
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(16),
+                                        bottomRight: Radius.circular(16)
+                                    )
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: 200,
+                          margin: EdgeInsets.only(right: 16),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: Colors.redAccent.withOpacity(0.4),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
 
                 ],
               ),
@@ -94,6 +214,21 @@ class _MainPageState extends State<MainPage> {
           ],
         ),
       ),
+
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
