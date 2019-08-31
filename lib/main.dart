e943747f4a97755110c8e679372dea41dc9c6d04
Dreamwalker <aristojeff@gmail.com>
2019-08-31 11:29:57 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_second_story/goals_app/main_page.dart';
+import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
 
-void main() => runApp(GoalsApp());
+void main() => runApp(ParticleApps());
 
 
--- lib/particle_screen/main_page.dart
+++ lib/particle_screen/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_particles/particles.dart';
+
 class ParticleApps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,7 +18,32 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            color: Colors.orange[50],
+            child: Particles(
+              30,
+              Colors.blue
+            )
+          ),
+          Container(
+              child: Particles(
+                  30,
+                  Colors.red
+              )
+          ),
+          Container(
+              child: Particles(
+                  30,
+                  Colors.green
+              )
+          )
+        ],
+      ),
+    );
   }
 }
 
