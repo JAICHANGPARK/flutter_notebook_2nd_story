b4bdc1ba898d9ff2c5b6523b02bc19f9e640b2be
Dreamwalker <aristojeff@gmail.com>
2019-09-01 19:45:04 +0900

    Update main_page.dart

--- lib/anicorn_watch_app/main_page.dart
+++ lib/anicorn_watch_app/main_page.dart
@@ -21,26 +21,20 @@ class _DetailPageState extends State<DetailPage> {
       body: Stack(
         children: <Widget>[
           Positioned(
-            top: 16,
+            top: 0,
             left: 0,
-            right: 16,
+            right: 8,
             child: Container(
               height: 280,
               decoration: BoxDecoration(
-                color: Colors.white,
-                gradient: LinearGradient(
-                  colors: [
+                  color: Colors.white,
+                  gradient: LinearGradient(colors: [
                     Colors.redAccent,
                     Colors.redAccent.withOpacity(0.8),
                     Colors.orange[200],
-                  ],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter
-                ),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(34)
-                )
-              ),
+                  ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(34))),
               child: Stack(
                 children: <Widget>[
                   Positioned(
@@ -48,9 +42,110 @@ class _DetailPageState extends State<DetailPage> {
                     top: 16,
                     bottom: 24,
                     child: Container(),
-
                   )
                 ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              width: 160,
+              decoration: BoxDecoration(color: Colors.grey[200]),
+            ),
+          ),
+          Positioned(
+            top: 16,
+            left: 16,
+            right: 16,
+            child: AppBar(
+              actions: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.add_shopping_cart),
+                )
+              ],
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              leading: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                ),
+                color: Colors.black,
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Positioned(
+            left: 24,
+            top: 120,
+            child: Container(
+              height: 300,
+              width: 120,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Series".toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 12
+                  ),),
+                  SizedBox(height: 4,),
+                  Text("000".toUpperCase(),
+                    style: TextStyle(
+                        fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  Spacer(),
+                  Text("size".toUpperCase(),
+                    style: TextStyle(
+                        fontSize: 12
+                    ),),
+                  SizedBox(height: 4,),
+                  Text("Small", style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                  ),),
+                  Spacer(),
+                  Text("price".toUpperCase(), style: TextStyle(
+                      fontSize: 12
+                  ),),
+                  SizedBox(height: 4,),
+                  Text("\$410".toUpperCase(),
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                    ),),
+                  Spacer(),
+                  Container(
+                    height: 54,
+                    width: 54,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(colors: [
+                        Colors.redAccent,
+                        Colors.orange[200],
+                      ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.red[50],
+                          blurRadius: 10,
+                          spreadRadius: 10
+                        )
+                      ]
+                    ),
+                    child: Center(
+                      child: Icon(Icons.add_shopping_cart,
+                      color: Colors.white,),
+                    ),
+                  )
+                ]
+
               ),
             ),
           )
@@ -59,10 +154,6 @@ class _DetailPageState extends State<DetailPage> {
     );
   }
 }
-
-
-
-
 
 
 
