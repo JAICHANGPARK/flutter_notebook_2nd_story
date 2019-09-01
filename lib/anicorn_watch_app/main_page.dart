c57b8b080c594ca82a94e67a96d69c374b0fe52a
Dreamwalker <aristojeff@gmail.com>
2019-09-01 19:31:53 +0900

    Add

---
+++ lib/anicorn_watch_app/main_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AnicornWatchApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DetailPage(),
+    );
+  }
+}
+
+class DetailPage extends StatefulWidget {
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
