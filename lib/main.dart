8b2dfaea968aea067179e3435cdcff245e752714
Dreamwalker <aristojeff@gmail.com>
2019-09-04 21:21:46 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/star_animation/main_page.dart';
+import 'package:flutter_notebook_second_story/food_delivery_app_03/main_page.dart';
 
 
-void main() => runApp(StarAnimationApp());
+void main() => runApp(FoodDeliveryApp03());
 
 
