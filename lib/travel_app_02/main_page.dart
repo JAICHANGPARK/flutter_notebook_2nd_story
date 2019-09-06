98aa3b6af1a5b59e154125af08e0eaddacadf50b
Dreamwalker <aristojeff@gmail.com>
2019-09-06 22:03:52 +0900

    Update main_page.dart

--- lib/travel_app_02/main_page.dart
+++ lib/travel_app_02/main_page.dart
@@ -17,19 +17,49 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey[50],
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SafeArea(
-              top: true,
-              left: true,
-              right: true,
-              child: Text("Collections", style: TextStyle(
-                color: Colors.black,
-
-              ),),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 32,
+              ),
+              SafeArea(
+                top: true,
+                left: true,
+                right: true,
+                child: Text("Collections", style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold
+                ),),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Container(
+                height: 52,
+                padding: EdgeInsets.only(left: 16),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                child: Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      icon: Icon(Icons.search),
+                      hintText: "Search place or list..."
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
