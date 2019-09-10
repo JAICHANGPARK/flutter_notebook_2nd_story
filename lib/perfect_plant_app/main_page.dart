a1cdee6992e8fa2c960439012939e072159deb50
Dreamwalker <aristojeff@gmail.com>
2019-09-11 01:16:03 +0900

    Update main_page.dart

--- lib/perfect_plant_app/main_page.dart
+++ lib/perfect_plant_app/main_page.dart
@@ -25,21 +25,45 @@ class _MainPageState extends State<MainPage> {
             right: 0,
             top: 0,
             child: Container(
-              height: MediaQuery.of(context).size.height / 2.5,
+              padding: EdgeInsets.only(left: 24),
+              height: MediaQuery.of(context).size.height / 2.8,
               decoration: BoxDecoration(
                   image: DecorationImage(
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.3), BlendMode.darken),
                       image: NetworkImage(
                           "https://cdn.pixabay.com/photo/2016/11/20/08/03/flowerpot-1842098_960_720.jpg"),
                       fit: BoxFit.cover)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                    size: 32,
+                  ),
+                  Text(
+                    "My Plants",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
             ),
           ),
           Positioned(
             left: 20,
             right: 20,
-            top: MediaQuery.of(context).size.height / 2.5 - 20,
+            top: MediaQuery.of(context).size.height / 2.8 - 20,
             child: Container(
               height: 38,
               margin: EdgeInsets.all(4),
+              padding: EdgeInsets.only(left: 16),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(24),
@@ -49,6 +73,18 @@ class _MainPageState extends State<MainPage> {
                         blurRadius: 4,
                         spreadRadius: 2)
                   ]),
+              child: Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.tealAccent,
+                      ),
+                      hintText: "Search for a plant",
+                      hintStyle: TextStyle(fontSize: 14)),
+                ),
+              ),
             ),
           )
         ],
@@ -56,24 +92,3 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
