7be44fe74d565bb8a7ee7f2aa9fe9d9c4bcc7028
Dreamwalker <aristojeff@gmail.com>
2019-09-10 12:21:22 +0900

    Update main_page.dart

--- lib/perfect_plant_app/main_page.dart
+++ lib/perfect_plant_app/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class PerfectPlantApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -20,20 +19,43 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: <Widget>[],
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2016/11/20/08/03/flowerpot-1842098_960_720.jpg"),
+                      fit: BoxFit.cover)),
+            ),
+          ),
+          Positioned(
+            left: 20,
+            right: 20,
+            top: MediaQuery.of(context).size.height / 2.5 - 20,
+            child: Container(
+              height: 38,
+              margin: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(24),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 4,
+                        spreadRadius: 2)
+                  ]),
+            ),
+          )
+        ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
 
 
 
