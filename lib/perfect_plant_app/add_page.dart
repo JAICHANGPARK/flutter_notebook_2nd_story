48a0cc24296f22b47f2e56a983e133f86da1cf08
Dreamwalker <aristojeff@gmail.com>
2019-09-12 18:57:52 +0900

    Update add_page.dart

--- lib/perfect_plant_app/add_page.dart
+++ lib/perfect_plant_app/add_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -11,7 +12,7 @@ class _AddPlantPageState extends State<AddPlantPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.only(top: 16, left: 24, right: 16, bottom: 24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -27,7 +28,65 @@ class _AddPlantPageState extends State<AddPlantPage> {
               color: Colors.black,
               fontSize: 24,
               fontWeight: FontWeight.bold
-            ),)
+            ),),
+            SizedBox(height: 16,),
+            Text("For beginners that don't know yet how to "
+                "properly take care of little plants we "
+                "recommend ther following plants",
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.7),
+              fontSize: 14,
+              letterSpacing: 1.4
+            ),),
+            SizedBox(height: 16,),
+            Text("Choose one to begin with.",
+              style: TextStyle(
+                  color: Colors.black.withOpacity(0.7),
+                  fontSize: 14,
+                  letterSpacing: 1.4
+              ),),
+            Container(
+              height: MediaQuery.of(context).size.height / 1.7,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(right: 16),
+                    width: MediaQuery.of(context).size.width - 100,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          top: 24,
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(colors: <Color>[
+                                Colors.teal[100],
+                                Colors.teal[300]
+                              ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight
+                              ),
+                              borderRadius: BorderRadius.circular(16)
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 16),
+                    width: MediaQuery.of(context).size.width - 100,
+                    decoration: BoxDecoration(
+                        color: Colors.red
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
