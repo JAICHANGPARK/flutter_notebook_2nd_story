9835bcfdf6e7e0ae802faf81c160de0f5732c104
Dreamwalker <aristojeff@gmail.com>
2019-09-13 11:13:32 +0900

    Update detail_page.dart

--- lib/perfect_plant_app/detail_page.dart
+++ lib/perfect_plant_app/detail_page.dart
@@ -14,15 +14,39 @@ class _DetailPageState extends State<DetailPage> {
     return Scaffold(
       body: Stack(
         children: <Widget>[
+
           Positioned(
             top: 0,
             left: 0,
             right: 0,
             bottom: MediaQuery.of(context).size.height / 2,
-            child: Placeholder(),
+            child: Hero(
+              tag: widget.imgIndex,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://cdn.pixabay.com/photo/2016/11/24/19/49/vase-1857081_960_720.png"),
+                        fit: BoxFit.fitHeight)),
+              ),
+            ),
           ),
           Positioned(
-
+              left: 16,
+              top: 36,
+              child: IconButton(
+                icon: Icon(Icons.arrow_back,),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              )
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            top: MediaQuery.of(context).size.height / 2 - 80,
+            child: Placeholder(),
           )
         ],
       ),
