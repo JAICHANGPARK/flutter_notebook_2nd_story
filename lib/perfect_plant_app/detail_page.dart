20c7e89fb269f900a737a9e6c70d93d57dbedc31
Dreamwalker <aristojeff@gmail.com>
2019-09-13 11:19:56 +0900

    Update detail_page.dart

--- lib/perfect_plant_app/detail_page.dart
+++ lib/perfect_plant_app/detail_page.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
   final String imgIndex;
+
   DetailPage({this.imgIndex});
+
   @override
   _DetailPageState createState() => _DetailPageState();
 }
 
 class _DetailPageState extends State<DetailPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-
           Positioned(
             top: 0,
             left: 0,
@@ -35,12 +35,13 @@ class _DetailPageState extends State<DetailPage> {
               left: 16,
               top: 36,
               child: IconButton(
-                icon: Icon(Icons.arrow_back,),
-                onPressed: (){
+                icon: Icon(
+                  Icons.arrow_back,
+                ),
+                onPressed: () {
                   Navigator.of(context).pop();
                 },
-              )
-          ),
+              )),
           Positioned(
             bottom: 0,
             left: 0,
@@ -49,41 +50,65 @@ class _DetailPageState extends State<DetailPage> {
             child: Container(
               margin: EdgeInsets.only(top: 24),
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(24),
-                  topRight: Radius.circular(24)
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 4,
-                    spreadRadius: 1
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 4,
+                        spreadRadius: 1)
+                  ]),
+              padding: EdgeInsets.only(left: 24, top: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Succulents",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: Text(
+                      "In general, Succulents are desert plants with thick,"
+                      "fleshy leaves. Some of them have spines and some none."
+                      "Agave is an example of a popular succulent, along with aloe and "
+                      "popular echeveria rosettes.",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          letterSpacing: 1.2,
+                          wordSpacing: 1.5),
+                    ),
+                  ),
+                  Spacer(),
+                  Text("How to take care of it".toUpperCase()),
+                  Container(
+                    height: 160,
+                    child: Placeholder(),
                   )
-                ]
+                ],
               ),
             ),
           ),
-          Positioned()
+          Positioned(
+            right: 24,
+            top: MediaQuery.of(context).size.height / 2 - 80,
+            child: FloatingActionButton(
+              child: Icon(
+                Icons.add,
+                size: 38,
+              ),
+              backgroundColor: Colors.teal,
+              onPressed: () {},
+            ),
+          )
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
