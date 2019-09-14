0c29f4fb6b45b7e1d19855ff8363c2e39fdda888
Dreamwalker <aristojeff@gmail.com>
2019-09-14 14:48:14 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
 import 'package:flutter_notebook_second_story/perfect_plant_app/main_page.dart';
 
-void main() => runApp(PerfectPlantApp());
+void main() => runApp(ParticleApps());
 
 
