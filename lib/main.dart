8b6983b298c2cde2b29be291246699643eb16609
Dreamwalker <aristojeff@gmail.com>
2019-09-16 22:10:35 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/discount_tour/main_page.dart';
 import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
+import 'package:flutter_notebook_second_story/yoga_app/main_page.dart';
 
-void main() => runApp(DiscountTour());
+void main() => runApp(YogaApp());
 
 
