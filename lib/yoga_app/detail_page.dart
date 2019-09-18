0d9074627f1cbfbb321bf056fcf9ad7bc0ad7635
Dreamwalker <aristojeff@gmail.com>
2019-09-18 20:35:15 +0900

    Adds

--- lib/yoga_app/detail_page.dart
+++ lib/yoga_app/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:video_player/video_player.dart';
 
 class DetailPage extends StatefulWidget {
@@ -9,20 +10,22 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   VideoPlayerController _controller;
   Future<void> _initVideoPlayerFuture;
+
   @override
   void initState() {
-    _controller = VideoPlayerController.network("https://gcs-vimeo.akamaized.net/exp=1568736803~acl=%2A%2F403559628.mp4%2A~hmac=19740d88c1c9cdec0db81c86a7074aeceb04372ae62cef340989a4b051ea8bc6/vimeo-prod-skyfire-std-us/01/2243/5/136216234/403559628.mp4");
+    _controller = VideoPlayerController.network(
+        "https://gcs-vimeo.akamaized.net/exp=1568820872~acl=%2A%2F1191266893.mp4%2A~hmac=de6073eb41a7f433cc6f411057ecddaf0846943c8dbf3470a511e005077cd911/vimeo-prod-skyfire-std-us/01/1938/12/309694396/1191266893.mp4");
     _initVideoPlayerFuture = _controller.initialize();
     _controller.setLooping(true);
     super.initState();
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
     _controller.dispose();
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -37,14 +40,14 @@ class _DetailPageState extends State<DetailPage> {
               height: MediaQuery.of(context).size.height / 1.5,
               child: FutureBuilder(
                 future: _initVideoPlayerFuture,
-                builder: (context, snapshot){
-                  if(snapshot.connectionState == ConnectionState.done){
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
                     _controller.play();
                     return AspectRatio(
                       aspectRatio: _controller.value.aspectRatio,
                       child: VideoPlayer(_controller),
                     );
-                  }else{
+                  } else {
                     return Center(
                       child: CircularProgressIndicator(),
                     );
@@ -57,35 +60,35 @@ class _DetailPageState extends State<DetailPage> {
             top: 48,
             left: 16,
             child: InkWell(
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).pop(true);
               },
               child: Container(
                 height: 38,
                 width: 38,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle
-                ),
+                decoration:
+                    BoxDecoration(color: Colors.white, shape: BoxShape.circle),
                 child: Center(
                   child: Icon(Icons.keyboard_arrow_left),
                 ),
               ),
             ),
+          ),
+          SlidingUpPanel(
+            minHeight: 360,
+            maxHeight: MediaQuery.of(context).size.height - 160,
+            panel: Center(
+              child: Text("Sliding Up Pannel"),
+            ),
+            borderRadius: BorderRadius.only(
+
+            ),
           )
         ],
       ),
-
-
     );
-
   }
 }
-
-
-
-
-
 
 
 
