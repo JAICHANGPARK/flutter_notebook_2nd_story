eb66d3e28dca4f24a601d89f5d373d4c726b4e83
Dreamwalker <aristojeff@gmail.com>
2019-09-19 20:24:04 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
+import 'package:flutter_notebook_second_story/travel_collection/main_page.dart';
 import 'package:flutter_notebook_second_story/yoga_app/main_page.dart';
 
-void main() => runApp(ParticleApps());
+void main() => runApp(TravelCollection());
 
 
