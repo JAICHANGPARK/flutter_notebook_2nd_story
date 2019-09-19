70f42b3450fd7348e1bfa88b49e9b8a17070cd1b
Dreamwalker <aristojeff@gmail.com>
2019-09-19 21:10:04 +0900

    Update main_page.dart

--- lib/travel_collection/main_page.dart
+++ lib/travel_collection/main_page.dart
@@ -60,7 +60,7 @@ class _MainPageState extends State<MainPage> {
               ),
             ),
             Container(
-              height: MediaQuery.of(context).size.height / 8,
+              height: MediaQuery.of(context).size.height / 10,
               padding: EdgeInsets.only(left: 24, bottom: 16),
               child: Align(
                   alignment: Alignment.bottomLeft,
@@ -74,7 +74,135 @@ class _MainPageState extends State<MainPage> {
             ),
             Container(
               height: MediaQuery.of(context).size.height / 2.5,
-              child: Placeholder(),
+              padding: EdgeInsets.only(left: 24),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                children: <Widget>[
+                  Container(
+                    width: 240,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          left: 4,
+                          top: 0,
+                          right: 8,
+                          bottom: 8,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 3,
+                                  spreadRadius: 2
+                                )
+                              ],
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://cdn.pixabay.com/photo/2019/02/22/08/36/japan-4013068_960_720.jpg"),
+                                    fit: BoxFit.cover,
+                                    colorFilter: ColorFilter.mode(
+                                        Colors.black26, BlendMode.darken)),
+                                color: Colors.blueGrey,
+                                borderRadius: BorderRadius.circular(8)),
+                          ),
+                        ),
+                        Positioned(
+                          top: 16,
+                          left: 0,
+                          child: Container(
+                            height: 38,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                color: Colors.blueGrey,
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(7),
+                                    bottomRight: Radius.circular(7))),
+                            child: Center(
+                              child: Text(
+                                "06 days 16 hours",
+                                style: TextStyle(
+                                    color: Colors.white, letterSpacing: 1.5),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 62,
+                          left: 0,
+                          child: Container(
+                            height: 38,
+                            width: 70,
+                            decoration: BoxDecoration(
+                                color: Colors.red[300],
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(7),
+                                    bottomRight: Radius.circular(7))),
+                            child: Center(
+                              child: Text("-20%", style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22
+                              ),),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 240,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          left: 4,
+                          top: 0,
+                          right: 8,
+                          bottom: 8,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black12,
+                                      blurRadius: 3,
+                                      spreadRadius: 2
+                                  )
+                                ],
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://cdn.pixabay.com/photo/2019/02/22/08/36/japan-4013068_960_720.jpg"),
+                                    fit: BoxFit.cover,
+                                    colorFilter: ColorFilter.mode(
+                                        Colors.black26, BlendMode.darken)),
+                                color: Colors.blueGrey,
+                                borderRadius: BorderRadius.circular(8)),
+                          ),
+                        ),
+                        Positioned(
+                          top: 16,
+                          left: 0,
+                          child: Container(
+                            height: 38,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(7),
+                                    bottomRight: Radius.circular(7))),
+                            child: Center(
+                              child: Text(
+                                "06 days 16 hours",
+                                style: TextStyle(
+                                    color: Colors.white, letterSpacing: 1.5),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
             Container(
               height: MediaQuery.of(context).size.height / 8,
