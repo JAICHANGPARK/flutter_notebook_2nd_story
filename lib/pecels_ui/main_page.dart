7f6edbe0c67ece192a9f3bb2076c32827b7f6745
Dreamwalker <aristojeff@gmail.com>
2019-09-22 14:53:33 +0900

    Update main_page.dart

--- lib/pecels_ui/main_page.dart
+++ lib/pecels_ui/main_page.dart
@@ -47,7 +47,9 @@ class _MainPageState extends State<MainPage> {
                   ],
                 ),
               ),
-              SizedBox(height: 12,),
+              SizedBox(
+                height: 12,
+              ),
               Container(
                 height: 280,
                 child: Row(
@@ -58,18 +60,44 @@ class _MainPageState extends State<MainPage> {
                         children: <Widget>[
                           Expanded(
                             flex: 5,
-                            child: Placeholder(),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.white, width: 4),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    "https://cdn.pixabay.com/photo/2019/09/12/18/34/krakow-4472321__340.jpg"
+                                  ),
+                                  fit: BoxFit.cover
+                                )
+                              ),
+                            ),
                           ),
                           Expanded(
                             flex: 4,
-                            child: Placeholder(),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.white, width: 4),
+                                image: DecorationImage(
+                                  image: NetworkImage("https://cdn.pixabay.com/photo/2019/07/16/20/48/dolomites-4342572__340.jpg"),
+                                  fit: BoxFit.cover
+                                )
+                              ),
+                            ),
                           )
                         ],
                       ),
                     ),
                     Expanded(
                       flex: 3,
-                      child: Placeholder(),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 4),
+                            image: DecorationImage(
+                                image: NetworkImage("https://cdn.pixabay.com/photo/2019/08/06/02/16/landscape-4387209__340.jpg"),
+                                fit: BoxFit.cover
+                            )
+                        ),
+                      ),
                     )
                   ],
                 ),
@@ -89,6 +117,11 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
+
+
+
+
+
 
 
 
