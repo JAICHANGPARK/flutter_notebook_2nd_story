7307546ce0b5138c14fc2bc96d62a7e28d336ed9
Dreamwalker <aristojeff@gmail.com>
2019-09-22 14:47:20 +0900

    Adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
-import 'package:flutter_notebook_second_story/travel_collection/main_page.dart';
-import 'package:flutter_notebook_second_story/yoga_app/main_page.dart';
+import 'package:flutter_notebook_second_story/pecels_ui/main_page.dart';
 
-void main() => runApp(TravelCollection());
+
+void main() => runApp(PixelsUIApp());
 
 
--- lib/pecels_ui/main_page.dart
+++ lib/pecels_ui/main_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class PixelsUIApp extends StatelessWidget {
@@ -19,35 +18,42 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 24,
-              right: 24,
-              top: 38
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-
-              ],
-            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24, top: 38),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: 48,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(Icons.menu),
+                    Container(
+                      height: 42,
+                      width: 42,
+                      decoration: BoxDecoration(
+                        color: Colors.teal[400],
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.photo_filter,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.search)
+                  ],
+                ),
+              )
+            ],
           ),
         ),
+      ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
