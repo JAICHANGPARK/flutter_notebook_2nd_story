baa8fd94fd856efb793341dfbd99c484bd3a40d8
Dreamwalker <aristojeff@gmail.com>
2019-09-23 22:50:58 +0900

    Update detail_page.dart

--- lib/pecels_ui/detail_page.dart
+++ lib/pecels_ui/detail_page.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 
 class UserProfilePage extends StatefulWidget {
   @override
@@ -12,7 +12,106 @@ class _UserProfilePageState extends State<UserProfilePage> {
     return Scaffold(
       body: Column(
         children: <Widget>[
-
+          SafeArea(
+            top: true,
+            left: true,
+            right: true,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                    },
+                  ),
+                  Text(
+                    "Dreamwalker",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    color: Colors.black,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            padding: EdgeInsets.only(left: 16, right: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 38,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[Text("11947"), Text("FOLLOWERS")],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[Text("26M"), Text("TOTAL VIEWS")],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[Text("2.3"), Text("TOTAL LIKES")],
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 24,
+            child: Text("Flutter Developer"),
+          ),
+          Container(
+            height: 48,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 16, right: 8),
+                    color: Colors.greenAccent,
+                    child: Center(
+                      child: Text(
+                        "FOLLOW",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 8, right: 16),
+                    color: Colors.blueAccent,
+                    child: Center(
+                      child: Text(
+                        "MESSAGE HIM",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
