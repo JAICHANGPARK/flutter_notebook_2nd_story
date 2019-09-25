7bc4a3ec9a8c730825f3f6f7a61c84c5ee371aea
Dreamwalker <aristojeff@gmail.com>
2019-09-25 20:10:07 +0900

    Update main_page.dart

--- lib/travel_around/main_page.dart
+++ lib/travel_around/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelAround extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,6 +8,7 @@ class TravelAround extends StatelessWidget {
     );
   }
 }
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -18,135 +18,133 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-          children: <Widget>[
+      body: Stack(
+        children: <Widget>[],
+      ),
+      bottomNavigationBar: _buildBottomWidget(context),
+    );
+  }
 
-          ],
+  Widget _buildBottomWidget(BuildContext context) {
+    return BottomAppBar(
+      elevation: 16,
+      child: Container(
+        height: 68,
+        padding: EdgeInsets.only(left: 24, right: 24),
+        decoration: BoxDecoration(
+          color: Colors.white,
         ),
-      bottomNavigationBar: BottomAppBar(
-        elevation: 16,
-        child: Container(
-          height: 68,
-          padding: EdgeInsets.only(left: 24, right: 24),
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 3,
-                    width: 48,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent
-                    ),
-                  ),
-                  Icon(Icons.location_on, color: Colors.blue,),
-                  Text("Location",style: TextStyle(
-                    color: Colors.blue
-                  ),),
-                  SizedBox(height: 4,)
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 3,
-                    width: 48,
-                    decoration: BoxDecoration(
-                        color: Colors.transparent
-                    ),
-                  ),
-                  Icon(Icons.open_in_browser, color: Colors.grey,),
-                  Text("Explorer",style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 12
-                  ),),
-                  SizedBox(height: 4,)
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 3,
-                    width: 48,
-                    decoration: BoxDecoration(
-                        color: Colors.transparent
-                    ),
-                  ),
-                  Icon(Icons.calendar_today, color: Colors.grey,),
-                  Text("Plane",style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 12
-                  ),),
-                  SizedBox(height: 4,)
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 3,
-                    width: 48,
-                    decoration: BoxDecoration(
-                        color: Colors.transparent
-                    ),
-                  ),
-                  Icon(Icons.favorite_border, color: Colors.grey,),
-                  Text("Favorite",style: TextStyle(
-                      color: Colors.grey,
-                    fontSize: 12
-                  ),),
-                  SizedBox(height: 4,)
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 3,
-                    width: 48,
-                    decoration: BoxDecoration(
-                        color: Colors.transparent
-                    ),
-                  ),
-                  Icon(Icons.person_outline, color: Colors.grey,),
-                  Text("Explorer",style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 12
-                  ),),
-                  SizedBox(height: 4,)
-                ],
-              ),
-            ],
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 3,
+                  width: 48,
+                  decoration: BoxDecoration(color: Colors.blueAccent),
+                ),
+                Icon(
+                  Icons.location_on,
+                  color: Colors.blue,
+                ),
+                Text(
+                  "Location",
+                  style: TextStyle(color: Colors.blue),
+                ),
+                SizedBox(
+                  height: 4,
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 3,
+                  width: 48,
+                  decoration: BoxDecoration(color: Colors.transparent),
+                ),
+                Icon(
+                  Icons.open_in_browser,
+                  color: Colors.grey,
+                ),
+                Text(
+                  "Explorer",
+                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                ),
+                SizedBox(
+                  height: 4,
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 3,
+                  width: 48,
+                  decoration: BoxDecoration(color: Colors.transparent),
+                ),
+                Icon(
+                  Icons.calendar_today,
+                  color: Colors.grey,
+                ),
+                Text(
+                  "Plane",
+                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                ),
+                SizedBox(
+                  height: 4,
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 3,
+                  width: 48,
+                  decoration: BoxDecoration(color: Colors.transparent),
+                ),
+                Icon(
+                  Icons.favorite_border,
+                  color: Colors.grey,
+                ),
+                Text(
+                  "Favorite",
+                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                ),
+                SizedBox(
+                  height: 4,
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 3,
+                  width: 48,
+                  decoration: BoxDecoration(color: Colors.transparent),
+                ),
+                Icon(
+                  Icons.person_outline,
+                  color: Colors.grey,
+                ),
+                Text(
+                  "Explorer",
+                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                ),
+                SizedBox(
+                  height: 4,
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
