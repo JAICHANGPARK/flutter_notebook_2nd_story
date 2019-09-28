11d5493829bcf5265cc3dc1d3112cc8efaf85f36
Dreamwalker <aristojeff@gmail.com>
2019-09-28 15:45:53 +0900

    Update main_page.dart

--- lib/school_app/main_page.dart
+++ lib/school_app/main_page.dart
@@ -19,9 +19,95 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 2,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://cdn.pixabay.com/photo/2019/06/07/13/11/landscape-4258253_960_720.jpg"),
+                              fit: BoxFit.cover),
+                          color: Colors.red[100]),
+                      height: MediaQuery.of(context).size.height / 5,
+                    ),
+                  ),
+                  Positioned(
+                    left: 8,
+                    top: 48,
+                    right: 38,
+                    child: Container(
+                      height: 64,
+                      child: Row(
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(Icons.arrow_back_ios),
+                            onPressed: () {},
+                          ),
+                          Spacer(),
+                          Text(
+                            "School Events",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24),
+                          ),
+                          Spacer()
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height / 6.5,
+                    bottom: 0,
+                    left: 16,
+                    right: 16,
+                    child: Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24)),
+                      child: Container(
+                        padding: EdgeInsets.only(
+                            top: 24, left: 16, right: 16, bottom: 8),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(24)),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              height: 64,
+                              width: 64,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                      color: Colors.red[400], width: 2)),
+                              child: Center(
+                                child: Icon(
+                                  Icons.school,
+                                  size: 38,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
