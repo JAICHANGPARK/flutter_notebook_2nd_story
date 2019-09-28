b6f9fc83649f06d6097bd99af78a1141dea5561d
Dreamwalker <aristojeff@gmail.com>
2019-09-28 15:35:07 +0900

    Adds

---
+++ lib/school_app/main_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SchoolApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+
+      ),
+    );
+  }
+}
+
