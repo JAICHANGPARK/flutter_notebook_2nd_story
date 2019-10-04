7fc987ace6d621362fae5e93b051249d4a550902
Dreamwalker <aristojeff@gmail.com>
2019-10-04 23:32:31 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_second_story/fitness_app_set_running_goal/main_page.dart';
 import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
 import 'package:flutter_notebook_second_story/project_management_app/main_page.dart';
 
 
-void main() => runApp(ProjectManagementApp());
+void main() => runApp(FitnessSetRunningApp());
 
 
