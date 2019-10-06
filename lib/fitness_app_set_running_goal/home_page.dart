eb1eb6825c57e8da171ecf769d2f7aee33d7c76c
Dreamwalker <aristojeff@gmail.com>
2019-10-06 12:53:38 +0900

    Update home_page.dart

--- lib/fitness_app_set_running_goal/home_page.dart
+++ lib/fitness_app_set_running_goal/home_page.dart
@@ -14,16 +14,130 @@ class _HomePageState extends State<HomePage> {
         children: <Widget>[
           Expanded(
             flex: 2,
-            child: Placeholder(),
+            child: ListView(
+              children: <Widget>[
+                SizedBox(
+                  height: 48,
+                ),
+                Container(
+                  height: 160,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Text("12", style: TextStyle(
+                        color: Colors.redAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 58),),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text("MON", style: TextStyle(
+                            color: Colors.redAccent,
+                            fontSize: 12
+                          ),),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("13",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+
+                    ),),
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("14",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+
+                    ),),
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("15",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+
+                    ),),
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("16",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+
+                    ),),
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("17",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+
+                    ),),
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("18",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+
+                    ),),
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("19",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+
+                    ),),
+                  ),
+                ),
+              ],
+            ),
           ),
-         VerticalDivider(
-           color: Colors.white,
-         ),
+          VerticalDivider(
+            color: Colors.white.withOpacity(0.7),
+            width: 2,
+          ),
           Expanded(
             flex: 8,
-            child: Placeholder(),
+            child: Column(
+              children: <Widget>[],
+            ),
           ),
-
         ],
       ),
     );
