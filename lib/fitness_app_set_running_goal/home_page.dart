c300cf8454430b693c64384b594542f49e550955
Dreamwalker <aristojeff@gmail.com>
2019-10-06 12:45:18 +0900

    Update home_page.dart

--- lib/fitness_app_set_running_goal/home_page.dart
+++ lib/fitness_app_set_running_goal/home_page.dart
@@ -8,6 +8,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xff181E27),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Placeholder(),
+          ),
+         VerticalDivider(
+           color: Colors.white,
+         ),
+          Expanded(
+            flex: 8,
+            child: Placeholder(),
+          ),
+
+        ],
+      ),
+    );
   }
 }
