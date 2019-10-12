947f7f1433dd054ce6da961e31a4a9bf8cd693e3
Dreamwalker <aristojeff@gmail.com>
2019-10-12 20:48:14 +0900

    Create main_page.dart

---
+++ lib/exchange_finder/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ExchangeFinder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: EFMainPage(),
+    );
+  }
+}
+
+class EFMainPage extends StatefulWidget {
+  @override
+  _EFMainPageState createState() => _EFMainPageState();
+}
+
+class _EFMainPageState extends State<EFMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
