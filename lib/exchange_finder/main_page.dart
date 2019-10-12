a784aba8437872f5751a6b892565f765d2fc331f
Dreamwalker <aristojeff@gmail.com>
2019-10-12 20:52:16 +0900

    Update main_page.dart

--- lib/exchange_finder/main_page.dart
+++ lib/exchange_finder/main_page.dart
@@ -16,7 +16,7 @@ class EFMainPage extends StatefulWidget {
 
 class _EFMainPageState extends State<EFMainPage> {
   static const double appLeftPadding = 48.0;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,38 +31,17 @@ class _EFMainPageState extends State<EFMainPage> {
               width: MediaQuery.of(context).size.width / 2,
               child: Placeholder(),
             ),
+          ),
+          Positioned(
+            right: 24,
+            top: 16,
+            child: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
           )
-
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
