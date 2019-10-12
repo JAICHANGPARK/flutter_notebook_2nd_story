4405749bb2ab20d395d48060713b3e9a990dfe70
Dreamwalker <aristojeff@gmail.com>
2019-10-12 20:51:21 +0900

    Adds

--- lib/exchange_finder/main_page.dart
+++ lib/exchange_finder/main_page.dart
@@ -15,9 +15,54 @@ class EFMainPage extends StatefulWidget {
 }
 
 class _EFMainPageState extends State<EFMainPage> {
+  static const double appLeftPadding = 48.0;
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Positioned(
+            left: appLeftPadding,
+            top: 0,
+            child: Container(
+              height: 130,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Placeholder(),
+            ),
+          )
+
+        ],
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_second_story/fitness_app_set_running_goal/main_page.dart';
+import 'package:flutter_notebook_second_story/exchange_finder/main_page.dart';
 import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
-import 'package:flutter_notebook_second_story/project_management_app/main_page.dart';
 
-
-void main() => runApp(FitnessSetRunningApp());
+void main() => runApp(ExchangeFinder());
 
 
