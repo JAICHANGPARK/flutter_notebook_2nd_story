d33908f0a6e7dc5003eef083f214e485d3d4b131
Dreamwalker <aristojeff@gmail.com>
2019-10-13 17:10:08 +0900

    Adds Flutter toast

--- lib/exchange_finder/main_page.dart
+++ lib/exchange_finder/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_second_story/exchange_finder/models/curreny.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ExchangeFinder extends StatelessWidget {
   @override
@@ -131,57 +132,64 @@ class _EFMainPageState extends State<EFMainPage> {
                 height: MediaQuery.of(context).size.height / 1.7,
                 child: ListView.builder(
                   itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      padding: EdgeInsets.only(left: appLeftPadding),
-                      height: 82,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            height: 48,
-                            width: 48,
-                            padding: EdgeInsets.all(4),
-                            decoration:
-                                BoxDecoration(color: Colors.white, boxShadow: [
-                              BoxShadow(
-                                  color: Colors.black.withOpacity(0.15),
-                                  blurRadius: 3,
-                                  spreadRadius: 2,
-                                  offset: Offset(2, 2))
-                            ]),
-                            child: Image.network(
-                              priceItems[index].img,
-                              scale: 0.6,
-                              width: 40,
-                              height: 40,
+                    return InkWell(
+                      onTap: () {
+                        Fluttertoast.showToast(
+                            msg: priceItems[index].countryCode);
+                      },
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        padding: EdgeInsets.only(left: appLeftPadding),
+                        height: 82,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              height: 48,
+                              width: 48,
+                              padding: EdgeInsets.all(4),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.15),
+                                        blurRadius: 3,
+                                        spreadRadius: 2,
+                                        offset: Offset(2, 2))
+                                  ]),
+                              child: Image.network(
+                                priceItems[index].img,
+                                scale: 0.6,
+                                width: 40,
+                                height: 40,
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 32),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text("${priceItems[index].price}"),
-                                Text(priceItems[index].countryCode),
-                              ],
+                            Padding(
+                              padding: const EdgeInsets.only(left: 32),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text("${priceItems[index].price}"),
+                                  Text(priceItems[index].countryCode),
+                                ],
+                              ),
                             ),
-                          ),
-                          Spacer(),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 18),
-                            child: VerticalDivider(
-                              thickness: 2,
+                            Spacer(),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 18),
+                              child: VerticalDivider(
+                                thickness: 2,
+                              ),
                             ),
-                          ),
-                          IconButton(
-                            padding: EdgeInsets.zero,
-                            icon: Icon(Icons.more_vert),
-                            color: Colors.grey[200],
-                            onPressed: () {},
-                          )
-                        ],
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              icon: Icon(Icons.more_vert),
+                              color: Colors.grey[200],
+                              onPressed: () {},
+                            )
+                          ],
+                        ),
                       ),
                     );
                   },
