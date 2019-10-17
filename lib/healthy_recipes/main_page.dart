d216eda64579f2bf6513def80f56dd28de622218
Dreamwalker <aristojeff@gmail.com>
2019-10-17 22:01:03 +0900

    Update main_page.dart

---
+++ lib/healthy_recipes/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HealthyRecipes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ReciepesPage(),
+    );
+  }
+}
+
+class  RecipesPage extends StatefulWidget {
+  @override
+  _StateRecipesPage createState() => _StateRecipesPage();
+}
+
+class _StateRecipesPage extends State<RecipesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
