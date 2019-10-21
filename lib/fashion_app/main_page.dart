550aa751a069c96810fafedc28efea30af3972b3
Dreamwalker <aristojeff@gmail.com>
2019-10-21 22:53:42 +0900

    Create main_page.dart

---
+++ lib/fashion_app/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FashionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FashionHomePage(),
+    );
+  }
+}
+
+class FashionHomePage extends StatefulWidget {
+  @override
+  _FashionHomePageState createState() => _FashionHomePageState();
+}
+
+class _FashionHomePageState extends State<FashionHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
