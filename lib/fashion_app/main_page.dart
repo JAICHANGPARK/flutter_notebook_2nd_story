e0ba119a01a5acfd42dc27b9b7847afffe49bdeb
Dreamwalker <aristojeff@gmail.com>
2019-10-21 23:01:50 +0900

    Update main_page.dart

--- lib/fashion_app/main_page.dart
+++ lib/fashion_app/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class FashionApp extends StatelessWidget {
   @override
@@ -24,30 +25,69 @@ class _FashionHomePageState extends State<FashionHomePage> {
           children: <Widget>[
             Expanded(
               flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.menu),
-                    iconSize: 38,
-                    onPressed: (){},
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.search),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.menu),
                       iconSize: 38,
-                    onPressed: (){},
-                  )
-                ],
+                      onPressed: (){
+                        Fluttertoast.showToast(msg: "Pressed Menu Icon",
+                        backgroundColor: Colors.green);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.search),
+                        iconSize: 38,
+                      onPressed: (){
+                        Fluttertoast.showToast(msg: "Pressed Search Icon",
+                            backgroundColor: Colors.green);
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
             Expanded(
               flex: 1,
-              child: Placeholder(),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Fashion",style: TextStyle(
+                    fontSize: 38
+                  ),),
+                ),
+              ),
             ),
             Expanded(
               flex: 6,
-              child: Placeholder(),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(right: 24),
+                      width: MediaQuery.of(context).size.width - 120,
+                      decoration: BoxDecoration(
+                        color: Colors.red
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width - 120,
+                      decoration: BoxDecoration(
+                          color: Colors.red
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
+            Divider(height: 0,thickness: 2,),
             Expanded(
               flex: 1,
               child: Placeholder(),
