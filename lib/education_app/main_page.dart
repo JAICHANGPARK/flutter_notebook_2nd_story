9f17d664b916bf49d62ff4c93fb94ef57abf7a58
Dreamwalker <aristojeff@gmail.com>
2019-10-23 23:59:38 +0900

    Update main_page.dart

--- lib/education_app/main_page.dart
+++ lib/education_app/main_page.dart
@@ -1,3 +1,5 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EducationApp extends StatelessWidget {
@@ -21,6 +23,50 @@ class _MainPageState extends State<MainPage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
+            SafeArea(
+              top: true,
+              left: true,
+              right: true,
+              child: Container(
+                height: 80,
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("Home Page",style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                        RichText(text: TextSpan(
+                          children: [
+                            TextSpan(text: "Choose your course",style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12
+                            )),
+                            TextSpan(text: " right away",style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 12
+                            )),
+                          ]
+                        ),
+                        )
+                      ],
+                    ),
+                    Spacer(),
+                    Badge(
+                      child: Icon(Icons.notifications, color: Colors.teal,),
+                      showBadge: true,
+                      badgeContent: Text(""),
+
+                    )
+                  ],
+                ),
+              ),
+            )
 
           ],
         ),
