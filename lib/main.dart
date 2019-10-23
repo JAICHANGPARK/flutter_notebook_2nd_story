e53631226acf9372d3f3136761b4d70e9c36b856
Dreamwalker <aristojeff@gmail.com>
2019-10-23 23:42:12 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_second_story/fashion_app/main_page.dart';
 import 'package:flutter_notebook_second_story/healthy_recipes/main_page.dart';
+import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
 
-void main() => runApp(FashionApp());
+void main() => runApp(ParticleApps());
 
 
