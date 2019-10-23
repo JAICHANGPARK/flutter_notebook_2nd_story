db0d368954b85544228052c5d3f1f024d694cc6a
Dreamwalker <aristojeff@gmail.com>
2019-10-23 23:51:43 +0900

    Create main_page.dart

---
+++ lib/education_app/main_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class EducationApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
+
