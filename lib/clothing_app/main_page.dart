ca3aaa8b8b7ab79b8e734d70e707828e3cb3d674
Dreamwalker <aristojeff@gmail.com>
2019-10-28 22:13:28 +0900

    Update main_page.dart

--- lib/clothing_app/main_page.dart
+++ lib/clothing_app/main_page.dart
@@ -25,18 +25,19 @@ class _MainPageState extends State<MainPage> {
             left: 0,
             right: 0,
             top: 0,
-            bottom: MediaQuery.of(context).size.height / 2,
+            bottom: MediaQuery.of(context).size.height / 2.2,
             child: Container(
               padding:
                   EdgeInsets.only(left: 16, right: 16, top: 38, bottom: 32),
               decoration: BoxDecoration(
-                  color: Colors.red,
-                  image: DecorationImage(
-                    colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
-                      image: NetworkImage(
-                          "https://cdn.pixabay.com/photo/2016/11/16/10/28/two-girls-1828539_960_720.jpg"),
-                      fit: BoxFit.cover),),
-
+                color: Colors.red,
+                image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5), BlendMode.darken),
+                    image: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2016/11/16/10/28/two-girls-1828539_960_720.jpg"),
+                    fit: BoxFit.cover),
+              ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -102,13 +103,14 @@ class _MainPageState extends State<MainPage> {
           Positioned(
             left: 0,
             right: 0,
-            top: MediaQuery.of(context).size.height / 2.5,
+            top: MediaQuery.of(context).size.height / 2.55,
             bottom: 0,
             child: Container(
               decoration: BoxDecoration(
-                  color: Colors.blue,
+                  color: Colors.white,
                   borderRadius:
                       BorderRadius.only(topLeft: Radius.circular(24))),
+              child: Column(),
             ),
           )
         ],
@@ -122,3 +124,21 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
