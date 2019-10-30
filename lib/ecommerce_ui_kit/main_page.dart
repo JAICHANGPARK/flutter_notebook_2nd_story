dc5f4d301b45b1610ee47720b52acb2c0dbcb37d
Dreamwalker <aristojeff@gmail.com>
2019-10-30 23:45:00 +0900

    Adds

--- lib/ecommerce_ui_kit/main_page.dart
+++ lib/ecommerce_ui_kit/main_page.dart
@@ -10,7 +10,6 @@ class ECommerceUiKit extends StatelessWidget {
   }
 }
 
-
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -20,7 +19,36 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 1.8,
+              child: Placeholder(),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height / 5,
+              child: Placeholder(),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        elevation: 14,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.indigo,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), title: Text("Catalog")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), title: Text("Catalog")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), title: Text("Catalog")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), title: Text("Catalog")),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_second_story/clothing_app/main_page.dart';
+import 'package:flutter_notebook_second_story/ecommerce_ui_kit/main_page.dart';
 import 'package:flutter_notebook_second_story/education_app/main_page.dart';
 import 'package:flutter_notebook_second_story/particle_screen/main_page.dart';
 
-void main() => runApp(ClothingApp());
+void main() => runApp(ECommerceUiKit());
 
 
