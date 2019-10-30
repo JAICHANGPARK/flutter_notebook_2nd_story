08e88786b54dbd5a1d534bc5369f3636b7f630a4
Dreamwalker <aristojeff@gmail.com>
2019-10-30 23:39:16 +0900

    Update main_page.dart

--- lib/ecommerce_ui_kit/main_page.dart
+++ lib/ecommerce_ui_kit/main_page.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_second_story/audio_book_app/main_page.dart';
 
 class ECommerceUiKit extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(),
+    );
   }
 }
