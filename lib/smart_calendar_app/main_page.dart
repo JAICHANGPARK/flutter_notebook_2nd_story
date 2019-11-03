3c8580995d9a2a779d5cbf53d16de5702ab380a2
Dreamwalker <aristojeff@gmail.com>
2019-11-03 18:15:50 +0900

    Update main_page.dart

--- lib/smart_calendar_app/main_page.dart
+++ lib/smart_calendar_app/main_page.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+class SmartCalendarApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(),
+    ));
+  }
+}
