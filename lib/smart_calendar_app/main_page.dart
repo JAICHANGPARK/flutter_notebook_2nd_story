82a727fc8f787fa56fbfb8e3e7c8e8f32ab8e497
Dreamwalker <aristojeff@gmail.com>
2019-11-04 20:15:37 +0900

    Update main_page.dart

--- lib/smart_calendar_app/main_page.dart
+++ lib/smart_calendar_app/main_page.dart
@@ -34,31 +34,44 @@ class _HomePageState extends State<HomePage> {
                 children: <Widget>[
                   Row(
                     children: <Widget>[
-                      Text("19:30 IST",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18
-                      ),),
-                      Text("(GMT +1:30)",style: TextStyle(
-                        fontSize: 11,
-                      ),),
+                      Text(
+                        "19:30 IST",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      Text(
+                        "(GMT +1:30)",
+                        style: TextStyle(
+                          fontSize: 11,
+                        ),
+                      ),
                       Spacer(),
                       Icon(Icons.calendar_today),
-                      SizedBox(width: 16,),
+                      SizedBox(
+                        width: 16,
+                      ),
                       Icon(Icons.more_vert),
                     ],
                   ),
-                  SizedBox(height: 12,),
+                  SizedBox(
+                    height: 12,
+                  ),
                   Row(
                     children: <Widget>[
                       Icon(
                         Icons.add_location,
                         size: 12,
                       ),
-                      SizedBox(width: 8,),
-                      Text("New Delhi, India", style: TextStyle(
-                        fontSize: 13,
-                        color: Colors.grey,
-                      ),)
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        "New Delhi, India",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.grey,
+                        ),
+                      )
                     ],
                   )
                 ],
@@ -87,31 +100,41 @@ class _HomePageState extends State<HomePage> {
                 Expanded(
                   flex: 10,
                   child: Padding(
-                    padding: const EdgeInsets.only(top: 24, bottom: 0, right: 16, left: 16),
+                    padding: const EdgeInsets.only(
+                        top: 24, bottom: 0, right: 16, left: 16),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text("CHANGE IN TIMEZONE DETECTED!",style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 2
-                        ),),
+                        Text(
+                          "CHANGE IN TIMEZONE DETECTED!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 2),
+                        ),
                         Spacer(),
                         Row(
                           children: <Widget>[
-                            Text("09:00 CST",style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w800
-                            ),),
-                            SizedBox(width: 4,),
-                            Text("(GMT -6:30)", style: TextStyle(
-                              color: Colors.grey[100],
-                              fontSize: 10
-                            ),)
+                            Text(
+                              "09:00 CST",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              "(GMT -6:30)",
+                              style: TextStyle(
+                                  color: Colors.grey[100], fontSize: 10),
+                            )
                           ],
                         ),
-                        SizedBox(height: 8,),
+                        SizedBox(
+                          height: 8,
+                        ),
                         Row(
                           children: <Widget>[
                             Icon(
@@ -119,11 +142,13 @@ class _HomePageState extends State<HomePage> {
                               size: 12,
                               color: Colors.white,
                             ),
-                            SizedBox(width: 4,),
+                            SizedBox(
+                              width: 4,
+                            ),
                             Text(
                               "Texas, United States",
-                              style: TextStyle(color: Colors.grey,
-                              fontSize: 12),
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 12),
                             )
                           ],
                         ),
@@ -132,8 +157,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                Divider(color: Colors.white.withOpacity(0.3),
-                height: 0,),
+                Divider(
+                  color: Colors.white.withOpacity(0.3),
+                  height: 0,
+                ),
                 Expanded(
                   flex: 3,
                   child: Row(
@@ -151,22 +178,60 @@ class _HomePageState extends State<HomePage> {
                         flex: 1,
                         child: Container(
                           decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(8),
-                              bottomRight: Radius.circular(8),
-                              bottomLeft: Radius.circular(8),
-                            )
-                          ),
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(8),
+                                bottomRight: Radius.circular(8),
+                                bottomLeft: Radius.circular(8),
+                              )),
                           child: Center(
-                            child: Text("Update Timezone", style: TextStyle(
-                              color: Color(0xff1849EC),
-                              fontWeight: FontWeight.bold,
-                            ),),
+                            child: Text(
+                              "Update Timezone",
+                              style: TextStyle(
+                                color: Color(0xff1849EC),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
                         ),
                       )
                     ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 28,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Mon,",
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      " 27th Oct",
+                      style: TextStyle(fontSize: 32),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 9,
+                ),
+                Text(
+                  "3 Events",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
                   ),
                 )
               ],
@@ -177,16 +242,3 @@ class _HomePageState extends State<HomePage> {
     ));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
